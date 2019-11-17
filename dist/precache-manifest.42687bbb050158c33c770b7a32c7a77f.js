b187f296cbfc471f0ff5cf6333819ddbac69002e
owalid <othmane.el.ayadi@gmail.com>
2019-11-17 13:28:05 +0100

    prod

---
+++ dist/precache-manifest.42687bbb050158c33c770b7a32c7a77f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5efcf67bab473e5639",
+    "url": "/css/app.0aa4aab0.css"
+  },
+  {
+    "revision": "5a4014ca51d182a2862c",
+    "url": "/css/chunk-vendors.0ca4501c.css"
+  },
+  {
+    "revision": "5b5552e3110966a06685968c40efa862",
+    "url": "/img/logo.5b5552e3.png"
+  },
+  {
+    "revision": "196f9620603ff616c4131e10fe154502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c5efcf67bab473e5639",
+    "url": "/js/app.8b9490eb.js"
+  },
+  {
+    "revision": "5a4014ca51d182a2862c",
+    "url": "/js/chunk-vendors.90c54335.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
